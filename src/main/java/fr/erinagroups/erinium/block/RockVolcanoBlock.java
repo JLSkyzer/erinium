e2967cb596afeb7ee1476f6cc05013066c5a474a
JLSkyzer <killiandasneves4@gmail.com>
2023-04-28 17:36:31 +0200

    backup

--- src/main/java/fr/erinagroups/erinium/block/RockVolcanoBlock.java
+++ src/main/java/fr/erinagroups/erinium/block/RockVolcanoBlock.java
@@ -2,19 +2,33 @@
 package fr.erinagroups.erinium.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.gen.feature.template.RuleTest;
+import net.minecraft.world.gen.feature.template.IRuleTestType;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.Explosion;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.math.shapes.VoxelShapes;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Direction;
 import net.minecraft.state.StateContainer;
@@ -28,14 +42,15 @@ import net.minecraft.fluid.FluidState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.block.material.MaterialColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
 import java.util.stream.Stream;
+import java.util.Random;
 import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
@@ -43,17 +58,19 @@ import java.util.Collections;
 import java.util.AbstractMap;
 
 import fr.erinagroups.erinium.procedures.VolcanoRocksBreakProcedure;
+import fr.erinagroups.erinium.procedures.RockVolcanoAdditionalGenerationConditionProcedure;
 import fr.erinagroups.erinium.itemgroup.EriniumBlocksItemGroup;
-import fr.erinagroups.erinium.item.AlmonieGemItem;
 import fr.erinagroups.erinium.EriniumModElements;
 
 @EriniumModElements.ModElement.Tag
-public class VolcanoRockBlock extends EriniumModElements.ModElement {
-	@ObjectHolder("erinium:volcano_rock")
+public class RockVolcanoBlock extends EriniumModElements.ModElement {
+	@ObjectHolder("erinium:rock_volcano")
 	public static final Block block = null;
 
-	public VolcanoRockBlock(EriniumModElements instance) {
-		super(instance, 610);
+	public RockVolcanoBlock(EriniumModElements instance) {
+		super(instance, 745);
+		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
 	}
 
 	@Override
@@ -73,10 +90,10 @@ public class VolcanoRockBlock extends EriniumModElements.ModElement {
 		public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK, MaterialColor.RED).sound(SoundType.STONE).hardnessAndResistance(3f, 3f).setLightLevel(s -> 0)
-					.notSolid().setOpaque((bs, br, bp) -> false));
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
+					.setOpaque((bs, br, bp) -> false));
 			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
-			setRegistryName("volcano_rock");
+			setRegistryName("rock_volcano");
 		}
 
 		@Override
@@ -87,30 +104,6 @@ public class VolcanoRockBlock extends EriniumModElements.ModElement {
 		@Override
 		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
 			return 0;
-		}
-
-		@Override
-		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
-			Vector3d offset = state.getOffset(world, pos);
-			switch ((Direction) state.get(FACING)) {
-				case SOUTH :
-				default :
-					return VoxelShapes.or(makeCuboidShape(13, 0, 13, 4, 9, 4))
-
-							.withOffset(offset.x, offset.y, offset.z);
-				case NORTH :
-					return VoxelShapes.or(makeCuboidShape(3, 0, 3, 12, 9, 12))
-
-							.withOffset(offset.x, offset.y, offset.z);
-				case EAST :
-					return VoxelShapes.or(makeCuboidShape(13, 0, 3, 4, 9, 12))
-
-							.withOffset(offset.x, offset.y, offset.z);
-				case WEST :
-					return VoxelShapes.or(makeCuboidShape(3, 0, 13, 12, 9, 4))
-
-							.withOffset(offset.x, offset.y, offset.z);
-			}
 		}
 
 		@Override
@@ -132,16 +125,11 @@ public class VolcanoRockBlock extends EriniumModElements.ModElement {
 		}
 
 		@Override
-		public MaterialColor getMaterialColor() {
-			return MaterialColor.RED;
-		}
-
-		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
-			return Collections.singletonList(new ItemStack(AlmonieGemItem.block));
+			return Collections.singletonList(new ItemStack(Blocks.AIR));
 		}
 
 		@Override
@@ -170,5 +158,61 @@ public class VolcanoRockBlock extends EriniumModElements.ModElement {
 							new AbstractMap.SimpleEntry<>("z", z))
 					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 		}
+	}
+
+	private static Feature<OreFeatureConfig> feature = null;
+	private static ConfiguredFeature<?, ?> configuredFeature = null;
+	private static IRuleTestType<CustomRuleTest> CUSTOM_MATCH = null;
+
+	private static class CustomRuleTest extends RuleTest {
+		static final CustomRuleTest INSTANCE = new CustomRuleTest();
+		static final com.mojang.serialization.Codec<CustomRuleTest> codec = com.mojang.serialization.Codec.unit(() -> INSTANCE);
+
+		public boolean test(BlockState blockAt, Random random) {
+			boolean blockCriteria = false;
+			if (blockAt.getBlock() == Blocks.AIR)
+				blockCriteria = true;
+			return blockCriteria;
+		}
+
+		protected IRuleTestType<?> getType() {
+			return CUSTOM_MATCH;
+		}
+	}
+
+	private static class FeatureRegisterHandler {
+		@SubscribeEvent
+		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
+			CUSTOM_MATCH = Registry.register(Registry.RULE_TEST, new ResourceLocation("erinium:rock_volcano_match"), () -> CustomRuleTest.codec);
+			feature = new OreFeature(OreFeatureConfig.CODEC) {
+				@Override
+				public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
+					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
+					boolean dimensionCriteria = false;
+					if (dimensionType == RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("erinium:moon")))
+						dimensionCriteria = true;
+					if (!dimensionCriteria)
+						return false;
+					int x = pos.getX();
+					int y = pos.getY();
+					int z = pos.getZ();
+					if (!RockVolcanoAdditionalGenerationConditionProcedure.executeProcedure(Stream
+							.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x),
+									new AbstractMap.SimpleEntry<>("y", y), new AbstractMap.SimpleEntry<>("z", z))
+							.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll)))
+						return false;
+					return super.generate(world, generator, rand, pos, config);
+				}
+			};
+			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 2)).range(100)
+					.square().func_242731_b(10);
+			event.getRegistry().register(feature.setRegistryName("rock_volcano"));
+			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("erinium:rock_volcano"), configuredFeature);
+		}
+	}
+
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		event.getGeneration().getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES).add(() -> configuredFeature);
 	}
 }
--- src/main/java/fr/erinagroups/erinium/procedures/RockVolcanoAdditionalGenerationConditionProcedure.java
+++ src/main/java/fr/erinagroups/erinium/procedures/RockVolcanoAdditionalGenerationConditionProcedure.java
@@ -8,27 +8,27 @@ import java.util.Map;
 import fr.erinagroups.erinium.block.MoonSandBlock;
 import fr.erinagroups.erinium.EriniumMod;
 
-public class VolcanoRockAdditionalGenerationConditionProcedure {
+public class RockVolcanoAdditionalGenerationConditionProcedure {
 
 	public static boolean executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("world") == null) {
 			if (!dependencies.containsKey("world"))
-				EriniumMod.LOGGER.warn("Failed to load dependency world for procedure VolcanoRockAdditionalGenerationCondition!");
+				EriniumMod.LOGGER.warn("Failed to load dependency world for procedure RockVolcanoAdditionalGenerationCondition!");
 			return false;
 		}
 		if (dependencies.get("x") == null) {
 			if (!dependencies.containsKey("x"))
-				EriniumMod.LOGGER.warn("Failed to load dependency x for procedure VolcanoRockAdditionalGenerationCondition!");
+				EriniumMod.LOGGER.warn("Failed to load dependency x for procedure RockVolcanoAdditionalGenerationCondition!");
 			return false;
 		}
 		if (dependencies.get("y") == null) {
 			if (!dependencies.containsKey("y"))
-				EriniumMod.LOGGER.warn("Failed to load dependency y for procedure VolcanoRockAdditionalGenerationCondition!");
+				EriniumMod.LOGGER.warn("Failed to load dependency y for procedure RockVolcanoAdditionalGenerationCondition!");
 			return false;
 		}
 		if (dependencies.get("z") == null) {
 			if (!dependencies.containsKey("z"))
-				EriniumMod.LOGGER.warn("Failed to load dependency z for procedure VolcanoRockAdditionalGenerationCondition!");
+				EriniumMod.LOGGER.warn("Failed to load dependency z for procedure RockVolcanoAdditionalGenerationCondition!");
 			return false;
 		}
 		IWorld world = (IWorld) dependencies.get("world");
